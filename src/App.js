66d86d8d52241071c9e5367c83a37f26b270bc37
Muhammad Umair <181370124@gift.edu.pk>
2022-11-23 23:52:27 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import { Favorite, MoreVert, Share } from "@mui/icons-material";
-import {
-  Avatar,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  IconButton,
-  Typography,
-} from "@mui/material";
-import { Box } from "@mui/material";
-import pk from "../src/Images/pk.jpg";
+// import { Favorite, MoreVert, Share } from "@mui/icons-material";
+// import {
+//   Avatar,
+//   Card,
+//   CardActions,
+//   CardContent,
+//   CardHeader,
+//   CardMedia,
+//   IconButton,
+//   Typography,
+// // } from "@mui/material";
+// import { Box } from "@mui/material";
+// import pk from "../src/Images/pk.jpg";
 import "./App.css";
+import "./App.css";
+
+import Chart from "./Components/Charts/Chart";
+import CountryPicker from "./Components/CountryPicker/CountryPicker";
+import Cards from "./Components/Cards/Cards";
 
 function App() {
   return (
-    <Box
-      display="flex"
-      justifyContent="space-between"
-      sx={{ width: "100%", display: { xs: "block", sm: "block" } }}
-    >
-      Corona-App
-    </Box>
+    <div className="container">
+      <Cards />
+      <Chart />
+      <CountryPicker />
+    </div>
   );
 }
 
---
+++ src/Components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CountryPicker = () => {
+  return (
+    <div>CountryPicker</div>
+  )
+}
+
+export default CountryPicker
