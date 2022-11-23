d1d1c2873d1857c1ecc81b29877f83f316ad8463
Muhammad Umair <181370124@gift.edu.pk>
2022-11-24 02:58:52 +0500

    Api Data Fetching

--- src/App.js
+++ src/App.js
@@ -12,20 +12,27 @@
 // import { Box } from "@mui/material";
 // import pk from "../src/Images/pk.jpg";
 import "./App.css";
-import "./App.css";
 
 import Chart from "./Components/Charts/Chart";
 import CountryPicker from "./Components/CountryPicker/CountryPicker";
 import Cards from "./Components/Cards/Cards";
+import { fetchData } from "./Api/index";
+import React from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <Cards />
-      <Chart />
-      <CountryPicker />
-    </div>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+  render() {
+    return (
+      <div className="container">
+        <Cards />
+        <Chart />
+        <CountryPicker />
+      </div>
+    );
+  }
 }
 
 export default App;
